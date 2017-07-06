c47d5ca6097ba8fa973e374c360d5057d52f5321
lss <81438234@qq.com>
2017-07-06 21:25:24 +0800

    第一次提交

---
+++ demo1.vue
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<script type="text/javascript">
+		function () {
+		  let a = 10;
+		  var a = 1;
+		}
+	</script>
+</body>
+</html>
